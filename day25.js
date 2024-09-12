d2a06963d5deb3963335ff7ff6bc33b793ad472c
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2024-09-12 10:05:55 +0530

    Update day25.js

--- day25.js
+++ day25.js
@@ -1,18 +1,12 @@
 //print hollowSquarePrint 
-
-
 function hollowSquarePrint(num){
   for (let i = 1; i <= num; i++) {
     let row = " ";
     for (let j = 1; j <= num; j++) {
       if(i === 1 || i === num || j === 1 || j === num){
-        row += ' *'
-      }
+        row += ' *' }
       else{
-        row += '  '
-      }
+        row += '  ' }
     }
-    console.log(row);
-  }
-}
+    console.log(row);}}
 hollowSquarePrint(5);
