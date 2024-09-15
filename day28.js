dd03da794ad1af329a476de5636f4b94c2ef77ea
Aditya kumar <adityac22f@gmail.com>
2024-09-15 16:22:59 +0530

    x = o

--- day28.js
+++ day28.js
@@ -1,15 +1,15 @@
 // print prime numner 
 
 function primeNumber(num){
-  if (num < 1) {
-    return (`${num} is not a prime numner`)
-  }
-  for (let i = 2; i < num; i++) {
-    if (num%i === 0) {
-        return (`${num} is not a prime numner`)
+    if(num < 1){
+        return console.log(`it's prime number ${num}`);
     }
-  }
-  return (`${num} is a prime numner`)
+    for (let i = 2; i < num; i++) {
+        if (num%i===0) {
+            return (`it's not prime number ${num}`); 
+        }
+    }
+    return(`it's prime number ${num}`);
 }
 console.log(primeNumber(2));
 console.log(primeNumber(12));
